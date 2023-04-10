b3f91c2b73eaef43f03125b3bd77c790c9f4c701
Rohit-Agrawal3 <rohit.agrawal3@ibm.com>
2023-04-10 13:42:40 +0530

    Test auto trigger

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -39,8 +39,6 @@ function updateTargetRevisionInAppset() {
     git clone --branch "${configurationBranch}" "https://${GIT_TOKEN:?}@github.com/${TRAVIS_REPO_SLUG}.git"
     cd ./*
 
-
-
     TRAVIS_COMMIT=${gitlog}
     echo "TRAVIS_COMMIT = $TRAVIS_COMMIT"
     for appset_file in $appset_files; do
